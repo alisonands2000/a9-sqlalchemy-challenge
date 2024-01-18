3b813fe50a9db62cd2086da6438945b3c5098e9b
alisonands <131694547+alisonands@users.noreply.github.com>
2024-01-18 16:07:45 -0500

    update

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,22 +1,29 @@
 # Import the dependencies.
-
-
+from flask import Flask
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect, desc, func
 
 #################################################
 # Database Setup
 #################################################
-
+app = Flask(__name__)
 
 # reflect an existing database into a new model
-
 # reflect the tables
 
 
+
 # Save references to each table
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+# Base.prepare(autoload_with = engine)
 
+# measurement = Base.classes.measurement
+# station = Base.classes.station
 
-# Create our session (link) from Python to the DB
-
+# # Create our session (link) from Python to the DB
+# session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -28,3 +35,22 @@
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def main():
+    return "HEllo!"
+
+# @app.route("/api/v1.0/precipitation")
+
+
+# @app.route("/api/v1.0/stations")
+
+
+# @app.route("/api/v1.0/tobs")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# @app.route("/api/v1.0/<start>")
+
+
+# @app.route("/api/v1.0/<start>/<end>")
