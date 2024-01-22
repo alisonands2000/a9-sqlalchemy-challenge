fc838b65ecab0e761384d0b570c6a44a5c3ab873
alisonands <131694547+alisonands@users.noreply.github.com>
2024-01-22 14:54:25 -0500

    update

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 
 # Save references to each table
-engine = create_engine("sqlite:///Starter_Code/Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 Base = automap_base()
 Base.prepare(autoload_with = engine)
 
@@ -33,8 +33,8 @@ past_year = dt.datetime.strptime(date[0], '%Y-%m-%d') - dt.timedelta(365)
 
 past_year_data = session.query(measurement.date, measurement.prcp).\
     filter(measurement.date >= past_year).all()
-prcp_df = pd.DataFrame(past_year_data, columns = ['year', 'precipitation'])
-prcp_dict = prcp_df.to_dict()
+
+prcp_dict = {d:p for d, p in past_year_data}
 
 #################################################
 # Flask Setup
@@ -48,17 +48,24 @@ prcp_dict = prcp_df.to_dict()
 #################################################
 @app.route("/")
 def main():
-    return "HEllo!"
+    return '''
+        <h2>Available routes:</h2>
+        /api/v1.0/precipitation<br>
+        /api/v1.0/stations<br>
+        /api/v1.0/tobs<br>
+        /api/v1.0/[start] and /api/v1.0/[start]/[end]
+    '''
 
 @app.route("/api/v1.0/precipitation")
 def prcp():
-    return jsonify(prcp_dict)
+    print(prcp_dict)
+    return prcp_dict
 
 @app.route("/api/v1.0/stations")
 def stations():
     stations = session.query(station.station, station.name).all()
-    stations_list = [[station[0], station[1]] for station in stations]
-    return jsonify(stations_list)
+    stations_list = {id:loc for id, loc in stations}
+    return stations_list
 
 @app.route("/api/v1.0/tobs")
 def most_active_station():
@@ -69,8 +76,18 @@ def most_active_station():
     # most_active_station_past_year = most_active_station_latest_date - dt.timedelta(365)
     most_active_station_past_year_data = session.query(measurement.date, measurement.tobs).\
         filter(measurement.date >= past_year).all()
-    prcp_df = pd.DataFrame(most_active_station_past_year_data, columns = ['date', 'temp'])
-    return (f"The most active station is {most_active_station}", prcp_df.values.tolist())
+    # prcp_df = pd.DataFrame(most_active_station_past_year_data, columns = ['date', 'temp'])
+    tobs = {d:t for d,t in most_active_station_past_year_data}
+    return tobs
+
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+def date_range(start, end = '2017-08-23'):
+    session = Session(engine)
+
+    [min,avg,max] = session.query(func.min(measurement.tobs),func.avg(measurement.tobs),func.max(measurement.tobs)).\
+        filter((measurement.date>=start)&(measurement.date<=end)).first()
+    return {'Min':min,'AVG':avg,'Max':max}
 
 
 if __name__ == "__main__":
